2fb109d32a2f84733953b4707d6b2661ec7726ff
Giordano Díaz <diazgiordano@gmail.com>
2022-02-14 17:43:33 -0500

    Add users model through devise gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(email: 'jane.doe@example.com', password: '123456', password_confirmation: '123456')
+User.create(email: 'john.doe@example.com', password: '123456', password_confirmation: '123456')
